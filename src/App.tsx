1dbeb1c6026a37a952f0d6d95927f399f21ef975
codepyrater <punna1s@cmich.edu>
2023-08-14 18:50:03 -0400

    fixed test

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         React wit typescript
-        </a>
-      </header>
+      <h1>Welcome to React</h1>
     </div>
   );
 }
