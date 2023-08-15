909d5924a3559cdb334782700e02bb50bfc608f6
codepyrater <punna1s@cmich.edu>
2023-08-14 23:31:54 -0400

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome message', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to React setup/i); // Update this line
-  expect(linkElement).toBeInTheDocument();
-});
